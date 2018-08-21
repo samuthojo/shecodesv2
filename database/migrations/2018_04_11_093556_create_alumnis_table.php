3953608e2ced4da0e540eeacc17ef65786d928db
samuthojo <samuthojo@gmail.com>
2018-08-21 07:34:47 +0300

    Solved data right truncated mysql error

--- database/migrations/2018_04_11_093556_create_alumnis_table.php
+++ database/migrations/2018_04_11_093556_create_alumnis_table.php
@@ -16,7 +16,7 @@ class CreateAlumnisTable extends Migration
         Schema::create('alumnis', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->string('year_finished');
             $table->timestamps();
             // $table->softDeletes();
